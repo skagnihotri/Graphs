f8dca11090d856c238c8f3df4e425eee658183a2
skagnihotri <skagnihotri1@gmail.com>
2020-08-28 19:55:22 +0530

    topological sort done

--- topological_sort.cpp
+++ topological_sort.cpp
@@ -51,6 +51,46 @@ public:
 
 		return ordering;
 	}
+
+	vector<string> TopologicalSort_bfs() {
+		unordered_map<string, int> indegree;
+
+		for (auto node : adjlist) {
+			indegree[node.first] = 0;
+		}
+
+		for (auto node : adjlist) {
+			// x --> y
+			for (auto nbr : node.second) {
+				indegree[nbr]++;
+			}
+		}
+
+		vector<string> ordering;
+		queue<string> q;
+		for (auto node : indegree) {
+			if (node.second == 0) {
+				q.push(node.first);
+			}
+		}
+
+		while (!q.empty()) {
+			auto node = q.front();
+			q.pop();
+
+			for (auto nbr : adjlist[node]) {
+				indegree[nbr]--;
+				if (indegree[nbr] == 0) {
+					q.push(nbr);
+				}
+			}
+
+			ordering.push_back(node);
+		}
+
+		return ordering;
+	}
+
 };
 
 
@@ -74,12 +114,18 @@ int main() {
 	g.display();
 
 	list<string> Tsort = g.TopologicalSort_dfs();
-	cout << "Topological Sort :-\n";
+	cout << "Topological Sort using DFS:-\n";
 	for (auto node : Tsort) {
 		cout << node << ", ";
 	}
 	cout << '\n';
 
+	vector<string> Tsort_bfs = g.TopologicalSort_bfs();
+	cout << "Topological Sort using BFS:-\n";
+	for (auto node : Tsort_bfs) {
+		cout << node << ", ";
+	}
+	cout << '\n';
 
 	return 0;
 }
